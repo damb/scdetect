9c10987d94e338323eacdafba5fa1531e915047f
Daniel Armbruster <dani.armbruster@gmail.com>
2022-07-22 22:50:33 +0000

    Define method

--- src/apps/cc/worker/event/command.cpp
+++ src/apps/cc/worker/event/command.cpp
@@ -7,6 +7,8 @@ namespace event {
 
 Command::Command(Type type) : _type{type} {}
 
+Command::Type Command::type() const { return _type; }
+
 }  // namespace event
 }  // namespace worker
 }  // namespace detect
